8f8d92b9209f6ccfd53f4124dcdcb64659c9800e
unknown <abdelrahmanjamal5565@gmail.com>
2022-01-04 15:51:12 +0200

    Calculating number of fingers with limited success

--- src/main.py
+++ src/main.py
@@ -55,44 +55,86 @@ def thresholdHandYCbCr(image):
         center_y - window_size : center_y + window_size,
     ]
 
+    # Detect the colors at the four corners, those colors are very likely to be a background
+    # corner_window = 5
+    # top_left_corner = (0, 0)
+    # top__right_corner = (0, image.shape[1] - 1)
+    # bot_left_corner = (image.shape[0] - 1, 0)
+    # bot_right_corner = (image.shape[0] - 1, image.shape[1] - 1)
+
+    # bg_clrs = []
+
+    # Get the colors at the four corners using the calculated points & corner window size
+    # One second
+    # top_left_clrs = image_hsv[
+    #     top_left_corner[0] : top_left_corner[0] + corner_window,
+    #     top_left_corner[1] : top_left_corner[1] + corner_window,
+    #     0
+    # ]
+    # top_right_clrs = image_hsv[
+    #     top__right_corner[0] : top__right_corner[0] + corner_window,
+    #     top__right_corner[1] - corner_window : top__right_corner[1],
+    #     0
+    # ]
+    # bot_left_crls = image_hsv[
+    #     bot_left_corner[0] - corner_window : bot_left_corner[0],
+    #     bot_left_corner[1] : bot_left_corner[1] + corner_window,
+    #     0
+    # ]
+    # bot_right_crls = image_hsv[
+    #     bot_right_corner[0] - corner_window : bot_right_corner[0],
+    #     bot_right_corner[1] - corner_window : bot_right_corner[1],
+    #     0
+    # ]
+
+    # top_left_clrs = np.mean(top_left_clrs)
+    # top_right_clrs = np.mean(top_right_clrs)
+    # bot_left_crls = np.mean(bot_left_crls)
+    # bot_right_crls = np.mean(bot_right_crls)
+
+    # bg_clrs.append(np.where(abs(image_hsv - top_left_clrs) <= 10))
+    # bg_clrs.append(np.where(abs(image_hsv - top_right_clrs) <= 10))
+    # bg_clrs.append(np.where(abs(image_hsv - bot_left_crls) <= 10))
+    # bg_clrs.append(np.where(abs(image_hsv - bot_right_crls) <= 10))
+
 
     #delta = 30 works well with red background
     delta = 40
 
     #points around the center point
-    center_x1 = center_x - delta
-    center_x2 = center_x + delta
-    center_y1 = center_y - delta
-    center_y2 = center_y + delta
+    # center_x1 = center_x - delta
+    # center_x2 = center_x + delta
+    # center_y1 = center_y - delta
+    # center_y2 = center_y + delta
 
 
-    #windows around the 4 new points, each 1 directional
-    skin_cr_x1 = image[
-        center_x1 - window_size : center_x1 + window_size,
-        center_y - window_size : center_y + window_size,
-        1,
-    ]
+    # #windows around the 4 new points, each 1 directional
+    # skin_cr_x1 = image[
+    #     center_x1 - window_size : center_x1 + window_size,
+    #     center_y - window_size : center_y + window_size,
+    #     1,
+    # ]
 
-    skin_cr_x2 = image[
-        center_x2 - window_size : center_x2 + window_size,
-        center_y - window_size : center_y + window_size,
-        1,
-    ]
+    # skin_cr_x2 = image[
+    #     center_x2 - window_size : center_x2 + window_size,
+    #     center_y - window_size : center_y + window_size,
+    #     1,
+    # ]
 
-    skin_cr_y1 = image[
-        center_x - window_size : center_x + window_size,
-        center_y1 - window_size : center_y1 + window_size,
-        1,
-    ]
+    # skin_cr_y1 = image[
+    #     center_x - window_size : center_x + window_size,
+    #     center_y1 - window_size : center_y1 + window_size,
+    #     1,
+    # ]
 
-    skin_cr_y2 = image[
-        center_x - window_size : center_x + window_size,
-        center_y2 - window_size : center_y2 + window_size,
-        1,
-    ]
+    # skin_cr_y2 = image[
+    #     center_x - window_size : center_x + window_size,
+    #     center_y2 - window_size : center_y2 + window_size,
+    #     1,
+    # ]
 
     # array to store skin colors to average and thrshold on
-    skin_colors = []
+    # skin_colors = []
     
     skin_cr = np.mean(skin_cr)
     skin_hsv = np.mean(skin_hsv)
@@ -100,33 +142,32 @@ def thresholdHandYCbCr(image):
     # skin_colors.append(skin_cr)
 
     # average values along x axis in positive direction and -ve direction, whicherver has the least difference take it
-    average_along_negative_x = np.mean(skin_cr_x1)
-    average_along_positive_x = np.mean(skin_cr_x2)
-    average_along_negative_y = np.mean(skin_cr_y1)
-    average_along_positive_y = np.mean(skin_cr_y2)
+    # average_along_negative_x = np.mean(skin_cr_x1)
+    # average_along_positive_x = np.mean(skin_cr_x2)
+    # average_along_negative_y = np.mean(skin_cr_y1)
+    # average_along_positive_y = np.mean(skin_cr_y2)
 
-    # skin difference = 5 works well with red background
-    skin_difference = 5
+    # # skin difference = 5 works well with red background
+    # skin_difference = 5
     
-    if abs(skin_cr - average_along_negative_x) < skin_difference :
-        skin_colors.append(average_along_negative_x)
+    # if abs(skin_cr - average_along_negative_x) < skin_difference :
+    #     skin_colors.append(average_along_negative_x)
         
-    if abs(skin_cr - average_along_positive_x) < skin_difference :
-        skin_colors.append(average_along_positive_x)
+    # if abs(skin_cr - average_along_positive_x) < skin_difference :
+    #     skin_colors.append(average_along_positive_x)
 
-    if abs(skin_cr - average_along_negative_y) < skin_difference :
-        skin_colors.append(average_along_negative_y)
+    # if abs(skin_cr - average_along_negative_y) < skin_difference :
+    #     skin_colors.append(average_along_negative_y)
 
-    if abs(skin_cr - average_along_positive_y) < skin_difference :
-        skin_colors.append(average_along_positive_y)
+    # if abs(skin_cr - average_along_positive_y) < skin_difference :
+    #     skin_colors.append(average_along_positive_y)
 
-    skin_cr = np.mean(skin_colors)
+    # skin_cr = np.mean(skin_colors)
 
 
 
     delta_cr = 10
     delta_hsv = 20
-
     skin_cr_threshold = cv2.inRange(
         image[:, :, 1], skin_cr - delta_cr, skin_cr + delta_cr
     )
@@ -135,6 +176,8 @@ def thresholdHandYCbCr(image):
     )
 
     se_window = 5
+    # for i in range(len(bg_clrs)):
+    #     skin_cr_threshold[bg_clrs[i][0], bg_clrs[i][1]] = 0
     skin_cr_threshold = closing(skin_cr_threshold, np.ones((se_window, se_window), np.uint8))
 
     # return cv2.bitwise_or(skin_cr_threshold,skin_hsv_threshold)
@@ -235,7 +278,7 @@ def get_num_fingers(
         dis_trans_list_j.remove(dis_trans_list_j[0])
     center_i = int(np.mean(dis_trans_list_i))
     center_j = int(np.mean(dis_trans_list_j))
-    difference_window = 40
+    # difference_window = 40
     if center_i >= dis_trans.shape[0] or center_j >= dis_trans.shape[1]:
         radius = dis_trans[palm_center_i, palm_center_j]
         test = disk((palm_center_i, palm_center_j), radius * 1.8, shape= dis_trans.shape)
@@ -243,7 +286,9 @@ def get_num_fingers(
         radius = dis_trans[center_i, center_j]
         test = disk((center_i, center_j), radius * 1.8, shape= dis_trans.shape)
     dis_trans = np.copy(thresholded)
+
     dis_trans[test] = 0
+    
     # dis_trans = cv2.Sobel(src=frame[ymin:ymax, xmin:xmax], ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5)
     # dis_trans = rgb2gray(dis_trans)
     # rr, cc = np.where(dis_trans != 0)
@@ -260,8 +305,8 @@ def get_num_fingers(
     # dis_trans = np.copy(thresholded)
     # dis_trans[rr, cc] = 0
     
-    #palm_peak_i, palm_peak_j = np.unravel_index(dis_trans_copy.argmin(), dis_trans.shape)
-    #rr, cc = line(palm_center_i, palm_center_j, palm_peak_i, palm_center_j)
+    # palm_peak_i, palm_peak_j = np.unravel_index(dis_trans_copy.argmin(), dis_trans.shape)
+    # rr, cc = line(palm_center_i, palm_center_j, palm_peak_i, palm_center_j)
     # dis_trans_list_i.append(palm_center_i)
     # dis_trans_list_j.append(palm_center_j)
     # if len(dis_trans_list_i) > dis_trans_window:
@@ -293,8 +338,17 @@ def get_num_fingers(
     # Contours describe the intersection points better than pixels
     # Because an intersection can be made up of more than 1 pixels
     contours, _ = findContours(
-        anded, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE
+        dis_trans, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE
     )
+
+    # Get the center of each contour remove the outlier
+    #  if there's more than one contour
+    contour_centers = []
+    if len(contours) > 1:
+        for cont in contours:
+            contour_centers.append((np.mean(int(cont[:, :, 0][0])), np.mean(int(cont[:, :, 1][0]))))
+
+    
 
     # Reject short branches that are the result of noise
     min_cnt_len = max(c, r) * 2
