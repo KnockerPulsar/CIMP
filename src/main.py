073595c2c9b31443506d546bdfccdcaccc98a2a4
Tarek Yasser <tarekspeed7@gmail.com>
2022-01-05 03:47:00 +0200

    More experiments with mean and cam shift. Now we capture a sample of the hand before starting to track to avoid hard-coding values

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ from time import time
 from scipy.ndimage import distance_transform_edt
 from skimage.morphology import skeletonize, closing, erosion
 from skimage.draw import ellipse_perimeter, circle_perimeter, disk
-from skimage import feature,exposure
+from skimage import feature, exposure
 import mediapipe as mp
 
 mp_hands = mp.solutions.hands
@@ -45,52 +45,19 @@ def main():
     # RGBA
 
     print(
-        "\nPress the number keys to view different stages of finger detection"
-        "\nThreshilding, skeletonization, anding, ording, etc..."
+        """
+        Press "e" with your hand inside the blue region to start tracking
+        Try to keep the background free of highlights or white objects
+        """
     )
-    ######## OpenCv background subtractor##################
-    #backSub = cv2.createBackgroundSubtractorKNN(history=30)
 
-    #framesList = []
-    while loop:
-        # To calculate FPS
-        start_time = time()  # time()
-
-        # Change what's shown inside the hand's bounding box
-
-        # Checck if the thread has a new frame
-
-        frame_available, frame = webcam.get_frame()
-        ###########################################
-        ######## manual background sub ############
-        ###########################################
-
-        # framesList.append(frame)
-        # if len(framesList):
-        # framesList.pop(0)
-        # framesList=np.array(framesList)
-        # mean_frame = (np.mean(framesList, axis=tuple(range(framesList.ndim - 1))) * 255).astype(np.uint8)
-        # frame = mean_frame
-        # frame = backSub.apply(frame)
-        ###########################################
-        ######### HOG descriptror #################
-        ###########################################
-        fd,frame = feature.hog(
-            frame,
-            orientations=8,
-            pixels_per_cell=(16, 16),
-            cells_per_block=(1, 1),
-            visualize=True,
-            channel_axis=-1,
-        )
-
-    term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+    term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 100, 10)
 
     ms_xstart = int(draw_buffer.shape[1] * 0.25)
     ms_ystart = int(draw_buffer.shape[0] * 0.25)
 
-    ms_width = int(draw_buffer.shape[1] * 0.2)
-    ms_height = int(draw_buffer.shape[0] * 0.2)
+    ms_width = int(draw_buffer.shape[1] * 0.3)
+    ms_height = int(draw_buffer.shape[0] * 0.4)
 
     x, y, w, h = (
         ms_xstart,
@@ -99,79 +66,95 @@ def main():
         ms_height,
     )
     track_window = (x, y, w, h)
+    roi_captured = False
 
-    with mp_hands.Hands(
-        model_complexity=0,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5,
-        static_image_mode=False,
-        max_num_hands=1,
-    ) as hands:  # Listens for mouse events
-        while loop:
-            # To calculate FPS
-            start_time = time()  # time()
+    
+    sizes = [180, 255]              # How large the the samples for hue and saturation are
+    ranges = [0, 180, 0, 255]       # The range of values for hue and saturation
+    channels = [0, 1]               # 0-> Hue, 1->Sat.
 
-            # Checck if the thread has a new frame
-            frame_available, frame = webcam.get_frame()
+    roi = None
+    roi_hist = None
 
-            # If there's no new frame, use the previous one
-            if not frame_available:
-                frame = prev_frame
+    while loop:
+        # To calculate FPS
+        start_time = time()  # time()
 
-            # flip frame
-            frame = cv2.flip(frame, 1)
-
-            pointer_pos_image_coordinates = (-1, -1)
-            draw_command = False
-            num_fingers = 0
-
-            # Get hand(s) bounding box
-            # Uses mediapipe's hand detector
-
-            # https://stackoverflow.com/questions/8593091/robust-hand-detection-via-computer-vision?noredirect=1&lq=1
-            # https://docs.opencv.org/3.4/da/d7f/tutorial_back_projection.html
-
-            hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            hs_frame = np.empty(hsv_frame.shape, dtype=np.uint8)
-            cv2.mixChannels(hsv_frame, hs_frame, [0, 0])
-            cv2.mixChannels(hsv_frame, hs_frame, [1, 1])
-
-            sizes = [180, 255]
-            ranges = [0, 180, 0, 255]
-            channels = [0, 1]
-
-            sat_thresh = 0
-            hsv_frame[np.where(hsv_frame[:, :, 1] < sat_thresh)] = (0, 0, 0)
-
-            hs_hist = cv2.calcHist(
-                hsv_frame, channels, None, sizes, ranges, accumulate=False
-            )
-            hs_hist = cv2.normalize(
-                hs_hist, hs_hist, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX
-            )
-
-            backproj = cv2.calcBackProject(
-                [hsv_frame], channels, hs_hist, ranges, scale=1
-            )
-
-            ret, track_window = cv2.CamShift(backproj, track_window, term_crit)
-
-            pts = cv2.boxPoints(ret)
-            pts = np.int0(pts)
-
-            frame = cv2.polylines(frame, [pts], True, 255, 2)
-
-            ################################################################################################
+        # Checck if the thread has a new frame
+        frame_available, frame = webcam.get_frame()
 
         # If there's no new frame, use the previous one
         if not frame_available:
             frame = prev_frame
 
-            # Paint the buffer on top of the base webcam image
-            # frame = overlay_images([frame, draw_buffer])
+        # flip frame
+        frame = cv2.flip(frame, 1)
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-            # Draw the image and UI
-            display_ui(frame, win_name, start_time, num_fingers, display_ui=True)
+        pointer_pos_image_coordinates = (-1, -1)
+        draw_command = False
+        num_fingers = 0
+
+        # https://stackoverflow.com/questions/8593091/robust-hand-detection-via-computer-vision?noredirect=1&lq=1
+        # https://docs.opencv.org/3.4/da/d7f/tutorial_back_projection.html
+
+        # Tracking mode
+        # Mean shift seems to be more stable than camshift
+        if roi_captured:
+           
+            hsv_frame[:,:,2] = 0
+
+            backproj = cv2.calcBackProject(
+                [hsv_frame], channels, roi_hist, ranges, scale=3
+            )
+
+            ret, track_window = cv2.meanShift(backproj, track_window, term_crit)
+
+            # Cam shift
+            # pts = cv2.boxPoints(ret)
+            # pts = np.int0(pts)
+            # frame = cv2.polylines(frame, [pts], True, 255, 2)
+            # if(pts.min() == pts.max() == 0):
+            #     roi_captured = False
+            
+            # Mean shift
+            x,y,w,h = track_window
+            frame = cv2.rectangle(frame, (x,y), (x+w,y+h), 255,2)
+
+
+        else:
+            # Capture a ROI to use as a search target later on
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("e"):
+                roi = hsv_frame[y : y + h, x : x + w]
+
+                # Remove the value channel
+                roi[:,:,2] = 0
+
+                # For some reason, doing the opposite of 
+                # "3. Threshold pixels with low saturation due to their instability."
+                # works better
+                mask = cv2.inRange(roi,(0,5,0) , (180,50,255))
+
+                roi_hist = cv2.calcHist(
+                    [roi], channels, mask, sizes, ranges, accumulate=False
+                )
+                cv2.normalize(
+                    roi_hist, roi_hist, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX
+                )
+                # Go into tracking mode
+                roi_captured = True
+
+            cv2.rectangle(frame, (x, y), (x + w, y + h), 255, 2)
+
+       
+        ################################################################################################
+
+        # Paint the buffer on top of the base webcam image
+        # frame = overlay_images([frame, draw_buffer])
+
+        # Draw the image and UI
+        display_ui(frame, win_name, start_time, num_fingers, display_ui=True)
 
         # Copy the frame for later use
         prev_frame = frame
